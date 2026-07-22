0b9e030247b4ebc5efc53124c84dd2b3641bb5ed
Mave <maverickmulyadi001@gmail.com>
2026-07-22 16:51:54 +0700

    Auto update 2026-07-22 16:51:54

--- server_mods/69 MinICruise/42.13/media/lua/shared/69mini_SpawnList.lua
+++
@@ -1,8 +0,0 @@
-if VehicleZoneDistribution then
-
-VehicleZoneDistribution.trailerpark.vehicles["Base.69minICruise"] = {index = -1, spawnChance = 3};
-VehicleZoneDistribution.trafficjams.vehicles["Base.69minICruise"] = {index = -1, spawnChance = 3};
-
-
-
-end
