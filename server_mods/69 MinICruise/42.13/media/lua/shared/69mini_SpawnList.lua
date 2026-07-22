107de567c7ecf44019a015ddb71610d9b682f661
Mave <maverickmulyadi001@gmail.com>
2026-07-22 15:39:56 +0700

    Auto update 2026-07-22 15:39:55

---
+++ server_mods/69 MinICruise/42.13/media/lua/shared/69mini_SpawnList.lua
@@ -0,0 +1,8 @@
+if VehicleZoneDistribution then
+
+VehicleZoneDistribution.trailerpark.vehicles["Base.69minICruise"] = {index = -1, spawnChance = 3};
+VehicleZoneDistribution.trafficjams.vehicles["Base.69minICruise"] = {index = -1, spawnChance = 3};
+
+
+
+end
---
+++ server_mods/69mini/42.0/media/lua/client/69mini_client.lua
@@ -0,0 +1,14 @@
+--***********************************************************
+--**                   KI5 / bikinihorst                   **
+--***********************************************************
+
+require "Hooks/DAMN_EnterAnimations";
+
+DAMN = DAMN or {};
+MINI69 = MINI69 or {};
+
+DAMN.EnterAnimations:registerVehicleScript("Base.69mini", "low");
+DAMN.EnterAnimations:registerVehicleScript("Base.69miniIJ", "low");
+DAMN.EnterAnimations:registerVehicleScript("Base.69miniMrB", "low");
+DAMN.EnterAnimations:registerVehicleScript("Base.69miniPS", "low");
+DAMN.EnterAnimations:registerVehicleScript("Base.69miniUnionJack", "low");
